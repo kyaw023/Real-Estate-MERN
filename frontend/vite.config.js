8f838391633f784334c6123fbc8a853d02a68410
kyaw023 <kyawkhainglynn023@gmail.com>
2024-08-31 16:08:54 +0630

    fixed error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+
   build: {
     outDir: "dist", // Make sure this is relative to the `frontend` directory
     assetsDir: "assets", // Ensure assets are being placed correctly
