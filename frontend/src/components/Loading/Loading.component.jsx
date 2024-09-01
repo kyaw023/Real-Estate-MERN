04a8bd4763fe7d5496441b080a4c7b58c7bd9532
kyaw023 <kyawkhainglynn023@gmail.com>
2024-09-02 00:20:14 +0630

    fixed loading state

--- frontend/src/components/Loading/Loading.component.jsx
+++ frontend/src/components/Loading/Loading.component.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { useTheme } from "next-themes";
-import "ldrs/square";
+import { Vortex } from "react-loader-spinner";
 
 const LoadingComponent = ({ children, isLoading }) => {
   const { theme } = useTheme();
   return isLoading ? (
     <div className="flex items-center justify-center h-screen">
-      <l-square
-        size={35}
-        stroke={5}
-        strokeLength={0.25}
-        bgOpacity={0.1}
-        speed={1.2}
-        color={theme === "dark" ? "white" : "black"}
-      ></l-square>
+      <Vortex
+        visible={true} // Set to true to show the loader
+        height="80" // Height of the loader
+        width="80" // Width of the loader
+        ariaLabel="vortex-loading" // Accessibility label
+        wrapperStyle={{}} // Optional: Additional styles for the wrapper
+        wrapperClass="vortex-wrapper" // Optional: Class name for the wrapper
+        colors={["red", "green", "blue", "yellow", "orange", "purple"]} // Colors for the vortex animation
+      />
     </div>
   ) : (
     children
