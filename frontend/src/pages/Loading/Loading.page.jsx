a535e78ba40bfb88c3dd1b4b3e084c5676b879d1
kyaw023 <kyawkhainglynn023@gmail.com>
2024-09-02 00:25:18 +0630

    fixed loading page

--- frontend/src/pages/Loading/Loading.page.jsx
+++ frontend/src/pages/Loading/Loading.page.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "ldrs/square";
 import { useTheme } from "next-themes";
+import { Vortex } from "react-loader-spinner";
 
 const LoadingPage = () => {
-  const { theme } = useTheme();
+  
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <l-square
-        size={35}
-        stroke={5}
-        strokeLength={0.25}
-        bgOpacity={0.1}
-        speed={1.2}
-        color={theme === "dark" ? "white" : "black"}
-      ></l-square>
+      <Vortex
+        visible={true} // Set to true to show the loader
+        height="80" // Height of the loader
+        width="80" // Width of the loader
+        ariaLabel="vortex-loading" // Accessibility label
+        wrapperStyle={{}} // Optional: Additional styles for the wrapper
+        wrapperClass="vortex-wrapper" // Optional: Class name for the wrapper
+        colors={["red", "green", "blue", "yellow", "orange", "purple"]} // Colors for the vortex animation
+      />
     </div>
   );
 };
